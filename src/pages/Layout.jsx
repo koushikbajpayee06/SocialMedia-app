26f5c389d000d73da2386ff1be33eeca2e0b6730
Koushik <Koushikbajpayee06@gmail.com>
2026-03-09 11:54:25 +0530

    sidebar compleated

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -10,14 +10,14 @@ const Layout = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   return user ?  (
     <div className='w-full flex h-screen'>
-      <Sidebar/>
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}/>
       <div className='flex-1 bg-slate-50'>
         <Outlet/>
       </div>
       {
         sidebarOpen ? 
         <X className='absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden' onClick={()=>setSidebarOpen(false)}/>
-        :<Menu className='absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden' onClick={()=>setSidebarOpen(true)}/>
+        : <Menu className='absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden' onClick={()=>setSidebarOpen(true)}/>
       }
     </div>
   ) : (
