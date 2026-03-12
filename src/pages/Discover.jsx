35884ef73355c37ac41283ad4c71c2f1b9a3342f
Koushik <Koushikbajpayee06@gmail.com>
2026-03-12 15:35:36 +0530

    discoveer page created

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {dummyConnectionsData} from '../assets/assets.js' 
+import { Search } from 'lucide-react'
+import UserCard from '../components/UserCard.jsx'
+import Loading from '../components/Loading.jsx'
 
 const Discover = () => {
+  const [input, setInput] = useState('')
+  const [users, setUsers] = useState(dummyConnectionsData)
+  const [loading, setLoading] = useState(false)
+
+  const handleSearch = async(e)=>{
+    if(e.key === "Enter"){
+      setUsers([])
+      setLoading(true)
+      setTimeout(()=>{
+        setUsers(dummyConnectionsData)
+        setLoading(false)
+      }, 1000)
+    }
+  }
   return (
-    <div>
-      
+    <div className='min-h-screen relative bg-slate-50'>
+      <div className='max-w-6xl mx-auto p-6'>
+        {/* Title */}
+        <div className='mb-8'>
+          <h1 className='text-3xl font-bold text-slate-900 mb-2'>Discover People</h1>
+          <p className='text-slate-600'>Connect with amazing people and grow your network</p>
+        </div>
+          {/* Search */}
+        <div className="mb-8 shadow-md border border-slate-200/60 bg-white/80">
+          <div className='p-6'>
+            <div className='relative'>
+              <Search className='absolute left-3 top-1/2 transform -translate-y-1/2 text-slate-400 w-5 h-5' size={20} />
+                <input
+                  type="text"
+                  placeholder="Search people by name, username, bio, or location..."
+                  className="pl-10 sm:pl-12 py-2 w-full border border-gray-300 rounded-md max-sm:text-sm"
+                  value={input}
+                  onChange={(e) => setInput(e.target.value)}
+                  onKeyDown={handleSearch}
+                />
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-wrap gap-6">
+            {users.map((user) => (
+              <UserCard key={user._id} user={user}  />
+            ))}
+          </div>
+          {
+            loading && (<Loading height='60vh'/>)
+          }
+         </div>
     </div>
   )
 }
